3754fb9eb405b52275422b88c13a29f0b278cc70
Horatiu Ion <horatiu.ion@gmail.com>
2018-08-28 21:06:30 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Sidebar/>
+  </div>
+</template>
+
+<script>
+import Sidebar from "./components/Sidebar";
+
+export default {
+  name: "App",
+  components: {
+    Sidebar
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: #ffffff;
+  margin-top: 10px;
+  margin-bottom: 5px;
+}
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,63 @@
+<template>
+<div id="sidebar">
+ <b-tabs id="sidebar-items" pills vertical >
+    <b-tab v-for="item in items" :key="item.id">
+      <template slot="title">
+        <SidebarItem v-bind:item="item"/>
+      </template>
+        <component :is="item.title"/>
+    </b-tab>
+  </b-tabs>
+</div>
+</template>
+
+<script>
+import SidebarItem from "./SidebarItem";
+import Home from "./Home";
+import btabs from "bootstrap-vue/es/components/tabs/tabs";
+import btab from "bootstrap-vue/es/components/tabs/tab";
+
+export default {
+  name: "Sidebar",
+  components: {
+    SidebarItem,
+    Home,
+    btabs,
+    btab
+  },
+  data() {
+    return {
+      items: [
+        {
+          id: 0,
+          title: "Home"
+        },
+        {
+          id: 1,
+          title: "Resume"
+        },
+        {
+          id: 2,
+          title: "Github"
+        },
+        {
+          id: 3,
+          title: "Contact"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+div.col-auto {
+  width: 300px;
+}
+#sidebar-item {
+  width: 100%;
+  align-content: left;
+  margin-left: 1px;
+  padding-left: 1px;
+}
+</style>
